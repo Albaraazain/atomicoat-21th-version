52145278525195b3ced31c98f097d0ffa4c4107d
Albaraazain <albaraazain33@gmail.com>
2025-01-05 02:23:23 +0300

    Remove unused import from recipe_creation_screen.dart to clean up the codebase

--- lib/features/recipes/screens/recipe_creation_screen.dart
+++ lib/features/recipes/screens/recipe_creation_screen.dart
@@ -2,7 +2,6 @@ import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import '../models/recipe.dart';
 import '../providers/recipe_provider.dart';
-import '../../../core/auth/providers/auth_provider.dart';
 
 class DarkThemeColors {
   static const Color background = Color(0xFF121212);
